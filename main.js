a0480f0f39848f0d1e09b9d11e2c3dc07aa4aa76
Maria de Lourdes Carvalho Chaves <95364349+maluchaves91@users.noreply.github.com>
2024-04-05 18:38:50 -0300

    atualização

--- main.js
+++ main.js
@@ -5,18 +5,10 @@ function redirecionarParaIndex() {
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 400) {
+    if (window.scrollY > 200) {
         nav.classList.add('navbarvermelho', 'shadow');
-        var textElements = nav.querySelectorAll('.colornav'); // Substitua 'colornav' pela classe real dos elementos de texto
-        textElements.forEach(function (element) {
-            element.style.color = 'white';
-        });
     } else {
         nav.classList.remove('navbarvermelho', 'shadow');
-        var textElements = nav.querySelectorAll('.colornav'); // Substitua 'colornav' pela classe real dos elementos de texto
-        textElements.forEach(function (element) {
-            element.style.color = ''; // Isso remove a cor definida, voltando ao estilo padrão
-        });
     }
 });
 
