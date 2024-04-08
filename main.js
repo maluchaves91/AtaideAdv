c3d92bc83114f63569caf8acdc88e034862ecece
Maria de Lourdes Carvalho Chaves <95364349+maluchaves91@users.noreply.github.com>
2024-04-07 21:27:48 -0300

    index.html pronto, falta ids no css

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function redirecionarParaIndex() {
-    window.location.href = "index.html#area";
-}
+// function redirecionarParaIndex() {
+//     window.location.href = "index.html#area";
+// }
 
-var nav = document.querySelector('nav');
+// var nav = document.querySelector('nav');
 
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 200) {
-        nav.classList.add('navbarvermelho', 'shadow');
-    } else {
-        nav.classList.remove('navbarvermelho', 'shadow');
-    }
-});
+// window.addEventListener('scroll', function () {
+//     if (window.scrollY > 200) {
+//         nav.classList.add('navbarvermelho', 'shadow');
+//     } else {
+//         nav.classList.remove('navbarvermelho', 'shadow');
+//     }
+// });
 
 
 
